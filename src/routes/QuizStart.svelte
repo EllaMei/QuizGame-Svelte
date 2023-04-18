c4133e7c00b4645e0fbf2c0901dbe586082fc1fa
Will <willtrentphillips@gmail.com>
2023-04-18 12:16:14 +1000

    Randomized answer order

--- src/routes/QuizStart.svelte
+++ src/routes/QuizStart.svelte
@@ -13,9 +13,29 @@
 
     // import questions from "./question_page.svelte";
 
+    $: answers =[
+        correct_answers[i],
+        incorrect_answers[i*3],
+        incorrect_answers[(i*3+1)],
+        incorrect_answers[(i*3+2)]
+    ]
+    let answers2;
+
+    function shuffle(a) {
+    var y, x, z;
+    for (z = a.length - 1; z > 0; z--) {
+        y = Math.floor(Math.random() * (z + 1));
+        x = a[z];
+        a[z] = a[y];
+        a[y] = x;
+    }
+    return a;
+    }
+
     // Function to start quiz
     function handleStartClick() {
         question_visibility = 1
+        answers2 = shuffle(answers)
     }
 
     // Function to leave quiz early
@@ -32,7 +52,13 @@
             i = 0
             question_visibility=0
         }
-        
+        answers =[
+            correct_answers[i],
+            incorrect_answers[i*3],
+            incorrect_answers[(i*3+1)],
+            incorrect_answers[(i*3+2)]
+        ]
+        answers2 = shuffle(answers)
     }
 </script>
 
@@ -45,7 +71,7 @@
 
 <!-- Shows Question page or Homepage -->
 {#if (question_visibility===1)}
-    <Question_page on:click={handleClickAnswer} questions={questions} correct_answers={correct_answers} incorrect_answers={incorrect_answers} i={i}/>
+    <Question_page on:click={handleClickAnswer} questions={questions} answers={answers2} i={i}/>
     <Exitquiz on:click={handleQuizExit}/>
 {:else}
     <Homepage on:click={handleStartClick}/>
--- src/routes/question_page.svelte
+++ src/routes/question_page.svelte
@@ -1,12 +1,13 @@
 <script>
     // Defining variables and allowing them to be filled by other components
     export let questions = ['In which suburb is the Digital Traineeship Programme being run?','Which Train Station is the closest to the Cremorne Campus of Kangan Institute?','What is the capital city of Australia?'];
-    export let correct_answers = ['Cremorne','Richmond','Canberra'];
-    export let incorrect_answers = ['South Yarra','Bendigo','Dandenong','Frankston','Cranburne','Berwick','Beaconsfield','Adelaide','Brisbane','Perth','Paris','Melbourne','Sydney','Newcastle'];
     export let i = 0
     
     //Importing component 
     import Answerblock from "./answerblock.svelte";
+
+    export let answers;
+
 </script>
 
 <style>
@@ -26,10 +27,5 @@ h2 {
         passing on on:click event to another component
         Giving values for the different buttons to show
      -->
-    <Answerblock on:click answer={ [
-        correct_answers[i],
-        incorrect_answers[i*3],
-        incorrect_answers[(i*3+1)],
-        incorrect_answers[(i*3+2)]
-    ]}/> 
+    <Answerblock on:click answer = {answers} /> 
     
